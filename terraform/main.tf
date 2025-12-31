9b077825b20fc089b46bfaf482557f1bc14667f4
sanvedlatkar <sanvedlatkar0@gmail.com>
2025-12-31 22:22:22 +0530

    terraform: configure remote s3 backend for shared state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,16 @@
+############################################
+# Terraform Configuration + Remote State
+############################################
+
 terraform {
   required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket  = "sanved-eventpipeline-dev-raw-data"
+    key     = "terraform/state.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+  }
 
   required_providers {
     aws = {
@@ -9,20 +20,20 @@ terraform {
   }
 }
 
-################################
+############################################
 # Provider
-################################
+############################################
 
 provider "aws" {
-  region = var.region
+  region = "ap-south-1"
 }
 
-################################
+############################################
 # S3 Buckets
-################################
+############################################
 
 resource "aws_s3_bucket" "raw_data" {
-  bucket = var.raw_data_bucket_name
+  bucket = "sanved-eventpipeline-dev-raw-data"
 }
 
 resource "aws_s3_bucket_versioning" "raw_data_versioning" {
@@ -34,37 +45,41 @@ resource "aws_s3_bucket_versioning" "raw_data_versioning" {
 }
 
 resource "aws_s3_bucket" "reports" {
-  bucket = var.reports_bucket_name
+  bucket = "sanved-eventpipeline-dev-reports"
 }
 
-################################
-# IAM Role for Lambdas
-################################
+############################################
+# IAM Role for Lambda
+############################################
 
 resource "aws_iam_role" "lambda_role" {
   name = "sanved-eventpipeline-dev-lambda-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
       }
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
 }
 
-################################
-# IAM Permissions (NO KMS)
-################################
+############################################
+# IAM Policies
+############################################
 
+# CloudWatch Logs
 resource "aws_iam_role_policy_attachment" "lambda_basic_logs" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# S3 Access Policy
 resource "aws_iam_policy" "lambda_s3_access" {
   name = "sanved-eventpipeline-dev-lambda-s3-access"
 
@@ -93,9 +108,9 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_attach" {
   policy_arn = aws_iam_policy.lambda_s3_access.arn
 }
 
-################################
-# Lambda: Ingest (AWS-managed KMS)
-################################
+############################################
+# Lambda: Ingest
+############################################
 
 resource "aws_lambda_function" "ingest_lambda" {
   function_name = "sanved-eventpipeline-dev-ingest-lambda"
@@ -104,7 +119,7 @@ resource "aws_lambda_function" "ingest_lambda" {
   role          = aws_iam_role.lambda_role.arn
   filename      = "${path.module}/../lambda/ingest.zip"
   timeout       = 10
-  memory_size  = 128
+  memory_size   = 128
 
   environment {
     variables = {
@@ -113,9 +128,9 @@ resource "aws_lambda_function" "ingest_lambda" {
   }
 }
 
-################################
+############################################
 # Lambda: Process
-################################
+############################################
 
 resource "aws_lambda_function" "process_lambda" {
   function_name = "sanved-eventpipeline-dev-process-lambda"
@@ -124,7 +139,7 @@ resource "aws_lambda_function" "process_lambda" {
   role          = aws_iam_role.lambda_role.arn
   filename      = "${path.module}/../lambda/process.zip"
   timeout       = 10
-  memory_size  = 128
+  memory_size   = 128
 
   environment {
     variables = {
@@ -133,9 +148,9 @@ resource "aws_lambda_function" "process_lambda" {
   }
 }
 
-################################
+############################################
 # Lambda: Report
-################################
+############################################
 
 resource "aws_lambda_function" "report_lambda" {
   function_name = "sanved-eventpipeline-dev-report-lambda"
@@ -144,7 +159,7 @@ resource "aws_lambda_function" "report_lambda" {
   role          = aws_iam_role.lambda_role.arn
   filename      = "${path.module}/../lambda/report.zip"
   timeout       = 10
-  memory_size  = 128
+  memory_size   = 128
 
   environment {
     variables = {
@@ -154,9 +169,9 @@ resource "aws_lambda_function" "report_lambda" {
   }
 }
 
-################################
-# EventBridge (Daily)
-################################
+############################################
+# EventBridge (Daily Report Trigger)
+############################################
 
 resource "aws_cloudwatch_event_rule" "daily_report_schedule" {
   name                = "sanved-eventpipeline-dev-daily-report"
